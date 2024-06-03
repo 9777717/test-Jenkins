2441c583bf9d9ca46ab383bf6250edd09d134079
yqt_zhengbinbo <you@example.com>
2024-06-03 20:00:24 +0800

    fix: test

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+<template>
+    <div>
+        <H1>Test Nuxt</H1>
+        <div>
+            <p>1</p>
+            <p>2</p>
+            <p>3</p>
+            <p>4</p>
+            <p>5</p>
+            <p>6</p>
+            <p>7</p>
+            <p>8</p>
+            <p>9</p>
+        </div>
+        <h2>jenkins test</h2>
+        <div>
+            <p v-for="item,index in 'jenkins'" :key='index'>{{ item }}</p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
